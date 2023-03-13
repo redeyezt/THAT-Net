81d820df3888a6fedc4f0b773f2dc6ba0161b3ea
redeyezt <66226226+redeyezt@users.noreply.github.com>
2023-03-13 16:12:25 +0800

    Update get_ jpg2flo _sh.py

--- get_ jpg2flo _sh.py
+++ get_ jpg2flo _sh.py
@@ -1,29 +1,19 @@
 import os, glob
 
 def write_sh(fl):
-    sh_file = 'flownet_dad_train_nega_3.sh'
+    sh_file = 'jpg2flo.sh'
     image_path = fl
     flow_path = str(fl)[0:-6]+'flow_out'
     if not os.path.exists(flow_path):###如果不存在flow_path，则创建flow_path
         os.mkdir(flow_path)
     content=\
-        'CUDA_VISIBLE_DEVICES=3 python main.py --inference --model FlowNet2 --number_gpu 1\
+        'CUDA_VISIBLE_DEVICES=0 python main.py --inference --model FlowNet2 --number_gpu 1\
         --inference_dataset_iext jpg \
         --save_flow --inference_dataset ImagesFromFolder \
         --resume /media/administrator/b11512ec-3b41-4ff9-8ab9-b048dc11432b/hongyanjin/flownet2-pytorch-master/checkpoints/FlowNet2_checkpoint.pth.tar \
         --save {} \
         --inference_dataset_root {}'.format(flow_path, image_path)
 
-
-    '''
-    content = 'CUDA_VISIBLE_DEVICES=2 python main.py --inference --model FlowNet2 \
-        --save_flow --inference_dataset ImagesFromFolder \
-        --resume /media/administrator/b11512ec-3b41-4ff9-8ab9-b048dc11432b/hongyanjin/flownet2-pytorch-master/checkpoints/FlowNet2_checkpoint.pth.tar \
-        --save /media/administrator/b11512ec-3b41-4ff9-8ab9-b048dc11432b/hongyanjin/flownet2-pytorch-master/output \
-        --inference_dataset_root /media/administrator/b11512ec-3b41-4ff9-8ab9-b048dc11432b/hongyanjin/flownet2-pytorch-master/data/000821 \
-        --inference_dataset_iext jpg \
-        --number_gpus 1
-    '''
 
     with open(sh_file, 'a') as f:
         f.write(content + '\n')
