9072f271852c4e02c2a4766871b7fa00cc0f508a
redeyezt <66226226+redeyezt@users.noreply.github.com>
2023-03-13 16:05:05 +0800

    Update get_mp42jpg_sh.py

--- get_mp42jpg_sh.py
+++ get_mp42jpg_sh.py
@@ -12,12 +12,6 @@ path_file_number = glob.glob('E:\\master\\cv2_video_fig\\shiyan/*.mp4')
 file_number = glob.glob(pathname='*.mp4')
 
 
-#print(path_file_number)
-#print(len(path_file_number))
-#print(path_file_number[0])
-#print('----------------------------------------')
-#print(str(path_file_number[0])[-10:-4])
-
 def get_each_frame(video_path):
     # 读取视频文件
     videoCapture = cv2.VideoCapture(video_path)
@@ -37,10 +31,6 @@ def get_each_frame(video_path):
             cv2.imwrite(address, frame)
             #print('save image:', i)
         success, frame = videoCapture.read()
-
-
-
-# get_each_frame(r"E:\master\cv2_video_fig\dataset\000348\000348.mp4")
 
 
 
